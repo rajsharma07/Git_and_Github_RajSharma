bdab00871ea044e7570cf51157c52b605e0113cd
Raj Sharma <rajrsharma2004@gmail.com>
2026-09-06 15:12:47 +0530

    Restore Item ID state after soft reset

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
 import json
 import os
 
-from flask import Flask, jsonify, render_template
+from dotenv import load_dotenv
+from flask import Flask, jsonify, render_template, request, url_for
+from pymongo import MongoClient
+
+load_dotenv()
 
 app = Flask(__name__)
+MONGO_URI = os.getenv("MONGO_URI")
+if(not MONGO_URI):
+    raise ValueError("MONGO_URI is not set please add in .env file")
+
+client = MongoClient(MONGO_URI)
+db = client["todo_database"]
+todos = db["todos"]
 
 DATA_FILE = os.path.join(os.path.dirname(__file__), "data.json")
 
 
 @app.route("/", methods=["GET"])
 def get_frontend():
-    return render_template("index.html")
+    return "<h1>My Todo App</h1>"
 
+@app.route("/submittodoitem", methods=["POST"])
+def submit_todo_item():
+    item_name = request.form.get("itemName")
+    item_description = request.form.get("itemDescription")
+
+    todo = {
+        "itemName": item_name,
+        "itemDescription": item_description
+    }
+
+    todos.insert_one(todo)
+
+    return jsonify({
+        "message": "To-Do item submitted successfully"
+    }), 201
 @app.route("/todo", methods=["GET"])
 def get_todo_page():
     return render_template("todo.html")
